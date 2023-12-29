dcf2102a8be4279a6f7bc4a6ffe48349fac07cbd
vvwvvwwvvw <wjddnr3304@gmail.com>
2023-12-29 15:38:09 +0900

    개인 프로젝트1

--- untitled/src/NewProject/Repository/RevenueRepositoty.java
+++ untitled/src/NewProject/Repository/RevenueRepositoty.java
@@ -23,19 +23,17 @@ public class RevenueRepositoty {
     }
 
     public int SellRevenue(String productName) {
-        int a = 0;
-        int b = 0;
         List<ProductDTO> productDTOList = new ArrayList<>();
         List<ReleaseDTO> releaseDTOList = new ArrayList<>();
         int i;
         for (i = 0; i < productDTOList.size(); i++) {
             if (productName.equals(productDTOList.get(i).getProductName())) {
-                a = productDTOList.get(i).getPurchasePrice();
+
 
             }
             for (int j = 0; j < releaseDTOList.size(); j++) {
                 if (productName.equals(releaseDTOList.get(i).getProductName())) {
-                    b = releaseDTOList.get(i).getSellPrice();
+
 
                 }
             }
